5367eb1d71c02f6157ef2d7a43332b5448dc1ae7
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2026-06-01 13:55:05 -0700

    extracting links and images

--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup, Tag 
+from urllib.parse import urljoin
 
 def get_heading_from_html(html: str) -> str:
  soup = BeautifulSoup(html, "html.parser")
@@ -13,3 +14,21 @@ def get_first_paragraph_from_html(html: str) -> str:
  else:
   first_paragraph = soup.find("p")
  return first_paragraph.get_text(strip=True) if isinstance(first_paragraph, Tag) else ""
+
+def get_urls_from_html(html, base_url): 
+ soup = BeautifulSoup(html, "html.parser")
+ all_links = []
+ for link in soup.find_all("a"): 
+  href = link.get("href")
+  if href is not None:
+    all_links.append(urljoin(base_url, href))
+ return all_links
+
+def get_images_from_html(html, base_url):
+ soup = BeautifulSoup(html, "html.parser")
+ images = []
+ for image in soup.find_all("img"):
+  img_link = image.get("src")
+  if img_link is not None:
+   images.append(urljoin(base_url, img_link))
+ return images
--- test_extract.py
+++ test_extract.py
@@ -1,5 +1,5 @@
 import unittest
-from extract import get_heading_from_html, get_first_paragraph_from_html
+from extract import get_heading_from_html, get_first_paragraph_from_html, get_urls_from_html, get_images_from_html
 
 class TestExtract(unittest.TestCase):
   def test_heading(self) -> None:
@@ -17,4 +17,84 @@ class TestExtract(unittest.TestCase):
     </body></html>'''
     actual = get_first_paragraph_from_html(input_body)
     expected = "Main paragraph."
+    self.assertEqual(actual, expected)
+
+  def test_get_first_paragraph_from_html_main_not_priority(self):
+    input_body = '''<html><body>
+        <p>Outside paragraph.</p>
+        <main>
+            <p>Main paragraph.</p>
+        </main>
+        <p>Another paragraph</p>
+    </body></html>'''
+    actual = get_first_paragraph_from_html(input_body)
+    expected = "Main paragraph."
+    self.assertEqual(actual, expected)
+
+  def test_get_first_paragraph_from_html_no_paragraphs(self):
+    input_body = '''<html><body>
+       
+    </body></html>'''
+    actual = get_first_paragraph_from_html(input_body)
+    expected = ""
+    self.assertEqual(actual, expected)
+  
+  def test_get_heading_from_html_h2_fallback(self) -> None:
+    input_body = "<html><body><h2>This is an h2 title</h2></body></html>"
+    actual = get_heading_from_html(input_body)
+    expected = "This is an h2 title"
+    self.assertEqual(actual, expected)
+  
+  def test_get_heading_from_html_whitespace(self) -> None:
+    input_body = "<html><body><h1>   Whitespace Title   </h1></body></html>"
+    actual = get_heading_from_html(input_body)
+    expected = "Whitespace Title"
+    self.assertEqual(actual, expected)
+
+  def test_get_first_paragraph_empty(self) -> None:
+    input_body = "<html><body><h1>No paragraphs here</h1></body></html>"
+    actual = get_first_paragraph_from_html(input_body)
+    expected =""
+    self.assertEqual(actual, expected)
+
+  def test_get_urls_from_html_absolute(self) -> None: 
+    input_url = "https://crawler-test.com"
+    input_body = '<html><body><a href="https://crawler-test.com"><span>Boot.dev</span></a></body></html>'
+    actual = get_urls_from_html(input_body, input_url)
+    expected = ["https://crawler-test.com"]
+    self.assertEqual(actual, expected)
+
+  def test_get_urls_from_html_multiple(self) -> None:
+     input_url = "https://crawler-test.com"
+     input_body = '<html><body><a href="https://crawler-test.com"><span>Boot.dev</span></a><a href="https://another-test.com"another">another link</a></body></html>'
+     actual = get_urls_from_html(input_body, input_url)
+     expected = ["https://crawler-test.com", "https://another-test.com"]
+     self.assertEqual(actual, expected)
+
+  def test_get_urls_from_html_empty(self) -> None:
+     input_url = "https://crawler-test.com"
+     input_body = '<html><body><span>Boot.dev</span></body></html>'
+     actual = get_urls_from_html(input_body, input_url)
+     expected = []
+     self.assertEqual(actual, expected)
+
+  def test_get_images_from_html_realtive(self) -> None: 
+    input_url = "https://crawler-test.com"
+    input_body = '<html><body><img src="/logo.png" alt="Logo"></body></html>'
+    actual = get_images_from_html(input_body, input_url)
+    expected = ["https://crawler-test.com/logo.png"]
+    self.assertEqual(actual, expected)
+
+  def test_get_images_from_html_multiple(self) -> None: 
+    input_url = "https://crawler-test.com"
+    input_body = '<html><body><img src="/logo.png" alt="Logo"><img src="/hello.png" alt="Hello"></body></html>'
+    actual = get_images_from_html(input_body, input_url)
+    expected = ["https://crawler-test.com/logo.png", "https://crawler-test.com/hello.png"]
+    self.assertEqual(actual, expected)
+
+  def test_get_images_from_html_empty(self) -> None: 
+    input_url = "https://crawler-test.com"
+    input_body = '<html><body>No logos for you!</body></html>'
+    actual = get_images_from_html(input_body, input_url)
+    expected = []
     self.assertEqual(actual, expected)
